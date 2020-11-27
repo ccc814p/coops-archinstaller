5654e7d5c6fb3e3121d5e2b7b2f2293d4f3dbac2
ccc814p <63892230+ccc814p@users.noreply.github.com>
2020-11-27 02:17:16 -0500

    Update setup1v.sh

--- setup1v.sh
+++ setup1v.sh
@@ -1,6 +1,6 @@
 cfdisk
 mkfs.ext4 /dev/vda1 ; mount /dev/vda1 /mnt
 mkswap /dev/vda2 ; swapon /dev/vda2
-pacstrap -S /mnt base linux linux-firmware sudo networkmanager vim grub base-devel git xorg xorg-xinit xorg-server gdm gnome-terminal gnome-control-center gnome-tweak-tool gedit nautilus
+pacstrap /mnt base linux linux-firmware sudo networkmanager vim grub base-devel git xorg xorg-xinit xorg-server gdm gnome-terminal gnome-control-center gnome-tweak-tool gedit nautilus
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
