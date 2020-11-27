3fc519c1c59d3d495d48ea42975617a3ec174014
ccc814p <63892230+ccc814p@users.noreply.github.com>
2020-11-27 02:15:51 -0500

    Update setup2v.sh

--- setup2v.sh
+++ setup2v.sh
@@ -3,4 +3,4 @@ hwclock --systohc
 date
 vim /etc/locale.gen ; locale-gen
 vim /etc/locale.conf ; vim /etc/hostname ; vim /etc/hosts
-grub-install --target=i386-pc /dev/sda ; grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=i386-pc /dev/vda ; grub-mkconfig -o /boot/grub/grub.cfg
